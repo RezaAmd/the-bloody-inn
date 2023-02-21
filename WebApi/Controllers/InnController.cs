aba52ff0c1928a5963d8db73632ed4a8d742ad7f
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-02-21 22:42:05 +0330

    🌱 Initialize Inn api endpoints (MOCK).

---
+++ WebApi/Controllers/InnController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBloodyInn.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class InnController : ControllerBase
+{
+    #region Ctor
+    #endregion
+
+    [HttpPost]
+    public async Task<IActionResult> Create() => Ok();
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => Ok();
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string id) => Ok();
+}
