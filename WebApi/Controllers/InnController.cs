36718f5b314f23629dfc66b3dd29426cd8c3ce97
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-02-24 22:03:39 +0330

    🌱 Add authorize to unn endpoints.

--- WebApi/Controllers/InnController.cs
+++ WebApi/Controllers/InnController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TheBloodyInn.WebApi.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]/[action]")]
 public class InnController : ControllerBase
--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿global using TheBloodyInn.Domain.Common;
+﻿global using MediatR;
 global using System.Text;
 global using TheBloodyInn.Application.Common.Models.DTOs.Settings;
+global using TheBloodyInn.Domain.Common;
