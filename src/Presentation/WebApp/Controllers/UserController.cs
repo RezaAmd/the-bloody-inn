0351be4f1b407fe352bc5497617fa3e570de1376
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-06-17 20:07:19 +0330

    ♻️ Change controller name.

---
+++ src/Presentation/WebApp/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿namespace TheBloodyInn.WebApp.Controllers;
+
+public class UserController : Controller
+{
+    #region Ctor
+
+    public UserController()
+    {
+
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public IActionResult Authentication()
+    {
+        return View();
+    }
+
+    #endregion
+}
