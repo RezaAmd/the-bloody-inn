41771fd6359a03c617a5ae6a1dda985700d22320
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-06-17 20:07:52 +0330

    🧹 Code Clean-up.

--- src/Presentation/WebApp/Controllers/HomeController.cs
+++ src/Presentation/WebApp/Controllers/HomeController.cs
@@ -1,45 +1,44 @@
 ﻿using System.Diagnostics;
 
-namespace TheBloodyInn.WebApp.Controllers
+namespace TheBloodyInn.WebApp.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    #region Properties
+
+    private readonly ILogger<HomeController> _logger;
+
+    #endregion
+
+    #region Ctor
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        #region Properties
-
-        private readonly ILogger<HomeController> _logger;
-
-        #endregion
-
-        #region Ctor
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        #endregion
-
-        #region Methods
-
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        #endregion
+        _logger = logger;
     }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    #endregion
 }
