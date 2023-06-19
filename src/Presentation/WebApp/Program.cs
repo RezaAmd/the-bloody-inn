4c9217fad0235a0d2eaaa0524b1f22efe33fc232
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-06-19 18:30:35 +0330

    ♻️ Refactor area route.

--- src/Presentation/WebApp/Program.cs
+++ src/Presentation/WebApp/Program.cs
@@ -69,12 +69,12 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 await app.RunAsync();
 
