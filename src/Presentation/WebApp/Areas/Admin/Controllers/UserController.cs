6b4ab760313bf0841e0e2e2bfb6bfeca59ded0c2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-06-22 22:36:28 +0330

    🌱 Initialize some controllers & Admin area. (MOCK)

---
+++ src/Presentation/WebApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿namespace TheBloodyInn.WebApp.Areas.Admin.Controllers;
+
+public class UserController : Controller
+{
+    #region Props
+
+
+
+    #endregion
+
+    #region Ctor
+
+
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #endregion
+}
