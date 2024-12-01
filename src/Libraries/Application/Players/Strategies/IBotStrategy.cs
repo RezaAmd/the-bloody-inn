26d778b9bfef3f88169db3d34c0127ed553368c4
Reza Ahmadi <r.ahmadi@rima.tax>
2024-12-01 09:45:33 +0330

    🧹 Remove bot strategies.

--- src/Libraries/Application/Players/Strategies/IBotStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using TheBloodyInn.Domain.Entities.Guests;
-
-namespace TheBloodyInn.Application.Players.Strategies
-{
-    public interface IBotStrategy
-    {
-        Task PerformActionAsync(BotPlayer bot, List<GuestEntity> availableGuests);
-    }
-}
